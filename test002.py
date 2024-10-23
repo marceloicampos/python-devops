5b431aa3432d389057da319202a9bd4fddd0bbe6
marceloicampos <marcelo.ildefonso@hotmail.com>
2024-10-23 14:58:18 -0300

    update test scripts on Python

--- test002.py
+++ test002.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#chmod -v +x test002.py
 ano_atual = int(input("Digite o ano atual: "))
 ano_nascimento = int(input("Digite o ano de nascimento: "))
 idade = ano_atual - ano_nascimento
